57f9377896101c82c042c4d50d9f4ecfef9642e8
Paula Eduarda Maciel <padumaciel@gmail.com>
2021-06-18 21:40:36 -0300

    Reparação de erros presentes no projeto na questão do botão de apagar a inserção e nos campos de data de publicação e quantidade de páginas na hora da limpeza do formulário

--- Exercício de Casa/script/script.js
+++ Exercício de Casa/script/script.js
@@ -8,26 +8,46 @@ function exibirDadosTabela(event){
     let qtdPaginasDoLivro = document.querySelector('#qtdPaginasDoLivro').value;
     let dataInsercaoLivro = new Date().toLocaleDateString('pt-br');
     let horarioInsercaoLivro = new Date().toLocaleTimeString('pt-br');
+    
 
     if(tituloDoLivro==="" ||  autorDoLivro==="" ||  isbnDoLivro==="" || dataDePublicacaoDoLivro==="" || qtdPaginasDoLivro==="" || dataInsercaoLivro===""){
         alert("Atenção! Dados inseridos de forma incorreta!")
     }else{
-        document.getElementById("corpo-tabela").innerHTML += `
+        document.getElementById("corpoTabela").innerHTML += `
             <tr id="tr-tabela">
                 <td>${tituloDoLivro}</td>
                 <td>${autorDoLivro}</td>
-                <td>${isbnDoLivro }</td>
+                <td>${isbnDoLivro}</td>
                 <td>${dataDePublicacaoDoLivro}</td>
                 <td>${qtdPaginasDoLivro}</td>
                 <td>${dataInsercaoLivro}</td>
                 <td>${horarioInsercaoLivro}</td>
-                <td><button id='botao-tabela' class='botao-tabela' onclick="removerLivroTabela(event)" ></button></td> 
+                <td><button id='botaoTabela' class='botao-tabela'></button></td> 
             </tr>
         `
+        limparFormulario();
     };
 }
 
-function removerLivroTabela(event){
-    document.getElementById("tr-tabela").remove();
+const limparFormulario = ()=>{
+    tituloDoLivro.value = ""
+    autorDoLivro.value = ""
+    isbnDoLivro.value = ""
+    dataDePublicacaoDoLivro.value = ""
+    dataInsercaoLivro = ""
+    qtdPaginasDoLivro.value = ""
 }
+
+const corpoTabela = document.querySelector('#corpoTabela');
+
+corpoTabela.addEventListener('click', (event)=>{
+    if (event.target.className=="botao-tabela"){
+        const botaoExcluirNovamente=event.target.parentElement
+        botaoExcluirNovamente.parentElement.remove()
+    }
+})
+
+// function removerLivroTabela(event){
+//     document.getElementById("tr-tabela").remove();
+// 
 
